75085619bef011f6e6f80006911615c967adf1f0
Ajay Dubey <ajay@jamdirect.ca>
2022-10-14 11:39:25 -0400

    Finishing the untie command.
    Working on skipping html tags in the tie command now

--- README.md
+++ README.md
@@ -11,4 +11,8 @@ Use a span tag with nonbreaking spaces around and between your selected words so
 * If you make multiple selections, only the last selection in the document will be treated.
 
 ## Keyboard shortcut binding
-If you wish to make use of keyboard shortcuts to run the command, please go to |files -> preference -> keyboard shortcuts|, search for "tie selected words" and add your own keyboard shortcut to it.
+If you wish to make use of keyboard shortcuts to run the command, please go to |files -> preference -> keyboard shortcuts|, search for _“tie selected words”_ and add your own keyboard shortcut to it.
+
+## Update OCT 2022
+* Added the ability to reverse the tying of words. Select a no-wrap span created by the extension and run the _“untie-selected-words”_ command to untie it. Add your own keyboard shortcut for this command to make it quicker for you.
+* Now your selection will ignore any html tags that might be in your selection.
--- extension.js
+++ extension.js
@@ -6,29 +6,52 @@ const vscode = require('vscode');
  */
 
 function activate(context) {
+	console.log("hey, this is working. The extension has been loaded.")
 
-	let disposable = vscode.commands.registerCommand('tie-selected-words.tie', function () {
+	let tie = vscode.commands.registerCommand('tie-selected-words.tie', function () {
 		
 		var editor = vscode.window.activeTextEditor;
 		if (!editor) {
 				return; 
 		}
 
+		// Lorem, ipsum dolor sit amet
+		// Lorem, ipsum <sup style="font-size: 9px; line-height: 0; vertical-align: 6px;">5</sup> dolor sit amet
+		
 		//getting the selected text
 		let selectedText = editor.document.getText(editor.selection);
 
 		// working with the string from here		
 		let NBSPd = selectedText.replace(/ /g,"&nbsp;");
 
-		let theNoSpan = `<span style="white-space:nowrap; display:inline-block; line-height:1.2em; mso-line-height-alt:0.6em">${NBSPd}</span>`
+		let withNoWrap = `<span style="white-space:nowrap; display:inline-block; line-height:1.2em; mso-line-height-alt:0.6em">${NBSPd}</span>`
 
 		editor.edit(editBuilder => {
-			editBuilder.replace(editor.selection, theNoSpan);
+			editBuilder.replace(editor.selection, withNoWrap);
 		});
 
 	});
-
-	context.subscriptions.push(disposable);
+	
+	let untie = vscode.commands.registerCommand('tie-selected-words.untie', function () {
+		
+		var editor = vscode.window.activeTextEditor;
+		if (!editor) {
+				return; 
+		}
+		
+		let selectedText = editor.document.getText(editor.selection);
+		
+		let spanRemoved = selectedText.replace(/<[^>]*>/g,"");
+		let nbspRemoved = spanRemoved.replace(/&nbsp;/g," ");
+		
+		editor.edit(editBuilder => {
+			editBuilder.replace(editor.selection, nbspRemoved);
+		});
+		
+	});
+	
+	context.subscriptions.push(tie);
+	context.subscriptions.push(untie);
 }
 
 // this method is called when your extension is deactivated
